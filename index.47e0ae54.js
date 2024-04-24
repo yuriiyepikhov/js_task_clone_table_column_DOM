b830e00e102b738e7d4f3812237f2d5eb3a5a33d
Yurii Yepikhov <yuriiyepikhov@gmail.com>
2024-04-24 20:26:30 +0300

    make build

---
+++ index.47e0ae54.js
@@ -0,0 +1,2 @@
+var r=document.querySelector("table").rows,e=!0,l=!1,t=void 0;try{for(var n,o=r[Symbol.iterator]();!(e=(n=o.next()).done);e=!0){var a=n.value,i=a.children[1];a.insertBefore(i.cloneNode(!0),a.children[4])}}catch(r){l=!0,t=r}finally{try{e||null==o.return||o.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.47e0ae54.js.map
